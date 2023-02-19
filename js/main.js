e900f901067bec75b7e2fed52ca44d99935ac4b3
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2023-02-19 23:39:17 +0200

    -f commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// task 1
+
+let nameHuman = prompt("Как тебя зовут?", "Name");
+let age = prompt("Сколько тебе лет?", 100);
+let city = prompt("Из какой ты страны?", "Ukraine");
+
+console.log(`Приветствую тебя ${nameHuman}, ${age} лет из ${city}`);
+document.write(`Приветствую тебя ${nameHuman}, <br> ${age} лет из ${city} <br>`);
+
+
+// task 2
+
+const number = [1, 2, 3, 4, 5];
+
+
+console.log(number.join(' ')); // 1 2 3 4 5
+document.write(number.join(' '));
+
+
+
+ 
